552502d931c9c9c1563368e148e2b11f507d8130
beasteve <beasteve43@gmail.com>
2023-02-23 14:44:16 +0100

    java

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World! Stephen");
+		
+		System.out.println(2*3);
+
+	}
+
+}
