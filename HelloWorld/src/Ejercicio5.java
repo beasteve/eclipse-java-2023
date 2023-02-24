3b1d35650054354b3d3e627d80ab793e82f89a20
beasteve <beasteve43@gmail.com>
2023-02-24 14:38:03 +0100

    last day friday of feb

---
+++ HelloWorld/src/Ejercicio5.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 int a = 5;
+		    int b = 10;
+		    int c = 3;
+		    int d = 8;
+		    
+		    if (a + b > c + d) {
+		      System.out.println("The sum of a and b is greater than the sum of c and d.");
+		    }
+		    
+		   
+
+	}
+
+}
